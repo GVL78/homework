411737958191364eee93ff25305ed2175c389857
GVL78 <georgy.laskov@gmail.com>
2026-06-13 16:35:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1748,7 +1748,6 @@ function generateExportHTML() {
 
   const isMath = state.currentSheet.subject.toLowerCase().includes("math") || state.currentSheet.subject.toLowerCase().includes("wiskunde");
   const mathjaxScript = isMath ? `
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
   ` : '';
 
